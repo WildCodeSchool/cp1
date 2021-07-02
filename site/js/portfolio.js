4305980f01950e98343ddd4e42d9db05e49ecae8
Anne-Marie Serveto <servetonguyen@outlook.com>
2021-07-02 12:48:35 +0200

    premier critere js

---
+++ site/js/portfolio.js
@@ -0,0 +1,12 @@
+const avatar = document.getElementById("avatar");
+
+avatar.onclick = function () {
+  avatar.src = "image/avatar.svg";
+};
+
+// button.addEventListener('click', () => {
+//     const name = document.getElementById('name');
+    
+//     let change = prompt('Choose the name');
+//     name.innerHTML = change;
+// })
