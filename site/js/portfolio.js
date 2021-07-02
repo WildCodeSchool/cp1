de4fc64e097c353e4104604c39367854c63948bb
Anne-Marie Serveto <servetonguyen@outlook.com>
2021-07-02 13:06:06 +0200

    Les trois premiers critères JS de fait

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,12 +1,21 @@
 const avatar = document.getElementById("avatar");
+const button = document.getElementById ("button");
+const background = document.querySelector('.pink-bg');
+const presentation = document.querySelector('.description pink-bg');
+const texte = document.querySelector('.pink-text');
 
 avatar.onclick = function () {
   avatar.src = "image/avatar.svg";
 };
 
-// button.addEventListener('click', () => {
-//     const name = document.getElementById('name');
-    
-//     let change = prompt('Choose the name');
-//     name.innerHTML = change;
-// })
+
+button.addEventListener("click", () => {
+  const name = document.getElementById("name");
+
+  let change = prompt ("Choose the name");
+  name.innerHTML = change;
+  name.style.color = "#FFF"
+  background.style.backgroundColor = "#750ff7";
+  presentation.style.backgroundColor ="#750ff7";
+  texte.style.color ="#750ff7";
+});
