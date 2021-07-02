1037c65508a959535f70cb0cb7a7ba706c2d5e1f
Anne-Marie Serveto <servetonguyen@outlook.com>
2021-07-02 11:05:09 +0200

    Modification des Algos 1 à 3

--- algo/2/isLeapYear.js
+++ algo/2/isLeapYear.js
@@ -14,7 +14,7 @@ function isLeapYear(year) {
   if ( year % 400 === 0 ){
     return true
   }
-  if else( year % 4 === 0 && year % 100 !== 0){}
+  else if ( year % 4 === 0 && year % 100 !== 0){
     return true
 }
   else {
