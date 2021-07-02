b685237846b71b52e7f85a2abe49ef4f9fddb415
Anne-Marie Serveto <servetonguyen@outlook.com>
2021-07-02 13:09:25 +0200

    tentative de réa du 3eme pour le double prompt

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,21 +1,30 @@
 const avatar = document.getElementById("avatar");
-const button = document.getElementById ("button");
-const background = document.querySelector('.pink-bg');
-const presentation = document.querySelector('.description pink-bg');
-const texte = document.querySelector('.pink-text');
+const button = document.getElementById("button");
+const background = document.querySelector(".pink-bg");
+const presentation = document.querySelector(".description pink-bg");
+const texte = document.querySelector(".pink-text");
 
 avatar.onclick = function () {
   avatar.src = "image/avatar.svg";
 };
 
+button.addEventListener("click", () => {
+  const name = document.getElementById("name");
+
+  const change = prompt("Choose the color");
+//   name.style.color = "#FFF";
+//   background.style.backgroundColor = "#750ff7";
+  //   presentation.style.backgroundColor ="#750ff7";
+  //   texte.style.color ="#750ff7";
+});
 
 button.addEventListener("click", () => {
   const name = document.getElementById("name");
 
-  let change = prompt ("Choose the name");
+  const change = prompt("Choose the name");
   name.innerHTML = change;
-  name.style.color = "#FFF"
+  name.style.color = "#FFF";
   background.style.backgroundColor = "#750ff7";
-  presentation.style.backgroundColor ="#750ff7";
-  texte.style.color ="#750ff7";
+  //   presentation.style.backgroundColor ="#750ff7";
+  //   texte.style.color ="#750ff7";
 });
